b01206d5b97decc526cf4abf17546f20c6353f40
Tadeu Augusto Dutra Pinto <tadeu.dutra@globant.com>
2025-09-06 04:50:04 -0300

    feat: add Role

---
+++ src/main/java/com/facint/taskmanager/model/Role.java
@@ -0,0 +1,26 @@
+package com.facint.taskmanager.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "roles")
+public class Role {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 30)
+    private RoleName name;
+
+}
