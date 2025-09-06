3dd897056a1d549ed05ea4059e1cbde23615c14a
Tadeu Augusto Dutra Pinto <tadeu.dutra@globant.com>
2025-09-06 05:45:26 -0300

    feat: add customized query using @Query

--- src/main/java/com/facint/taskmanager/repository/TaskRepository.java
+++ src/main/java/com/facint/taskmanager/repository/TaskRepository.java
@@ -3,6 +3,7 @@ package com.facint.taskmanager.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.facint.taskmanager.model.Task;
 
@@ -10,4 +11,7 @@ public interface TaskRepository extends JpaRepository<Task, Integer> {
 
     public List<Task> findByDescription(String description);
     public List<Task> findByDescriptionLike(String description);
+
+    @Query("select t from Task t inner join t.category c where c.name = ?1")
+    public List<Task> findByCategoryName(String categoryName);
 }
