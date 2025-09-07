4efca7eb59c040f4a7ab89d611fd334b089977a0
Tadeu Augusto Dutra Pinto <tadeu.dutra@globant.com>
2025-09-07 18:40:38 -0300

    dev: initial work done for hateos on TaskController class

--- src/main/java/com/facint/taskmanager/controller/TaskController.java
+++ src/main/java/com/facint/taskmanager/controller/TaskController.java
@@ -1,5 +1,9 @@
 package com.facint.taskmanager.controller;
 
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -7,11 +11,13 @@ import java.util.stream.Collectors;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +29,7 @@ import com.facint.taskmanager.service.TaskService;
 import jakarta.validation.Valid;
 
 @RestController
+@RequestMapping("/tasks")
 public class TaskController {
     
     @Autowired
@@ -31,7 +38,7 @@ public class TaskController {
     @Autowired
     private ModelMapper mapper;
 
-    @GetMapping("/tasks")
+    @GetMapping
     public List<TaskResponse> retrieveAllTasks(@RequestParam Map<String, String> params) {
 
         List<Task> tasks = new ArrayList<>();
@@ -51,16 +58,18 @@ public class TaskController {
         return tasksResponse;
     }
 
-    @GetMapping("/tasks/{id}")
-    public TaskResponse retrieveTaskById(@PathVariable Integer id) {
+    @GetMapping("/{id}")
+    public EntityModel<TaskResponse> retrieveTaskById(@PathVariable Integer id) {
 
         Task task = service.retrieveTaskById(id);
         TaskResponse taskResponse = mapper.map(task, TaskResponse.class);
+
+        EntityModel<TaskResponse> taskModel = EntityModel.of(taskResponse, linkTo(methodOn(TaskController.class).retrieveTaskById(id)).withSelfRel());
         
-        return taskResponse;
+        return taskModel;
     }
 
-    @PostMapping("/tasks")
+    @PostMapping
     public TaskResponse addTask(@Valid @RequestBody TaskRequest taskRequest) {
 
         Task task = mapper.map(taskRequest, Task.class);
@@ -68,7 +77,7 @@ public class TaskController {
         return mapper.map(service.saveTask(task), TaskResponse.class);
     }
 
-    @DeleteMapping("/tasks/{id}")
+    @DeleteMapping("/{id}")
     public void removeTask(@PathVariable Integer id) {
         service.removeById(id);
     }
